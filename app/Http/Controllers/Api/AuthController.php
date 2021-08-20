6bb8fe23bd33e3470fe80f3664b8c355f801b9d7
Syahrul Setiawan <monztergundul@gmail.com>
2021-08-20 21:36:42 +0700

    Initial commit + auth
    + basic auth for web and api (laravel passport)

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class AuthController extends BaseController
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $input = $request->all();
+        $input['passowrd'] = bcrypt($input['password']);
+        $user = User::create($input);
+
+        $success['token'] = $user->createToken('MyApp')->accessToken;
+        $success['name'] = $user->name;
+
+        return $this->sendResponse($success, 'User login successfully');
+    }
+
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['username' => $request->username, 'password' => $request->password])) {
+            $user = Auth::user();
+            $success['token'] = $user->createToken('MyApp')->accessToken;
+            $success['name'] = $user->name;
+
+            return $this->sendResponse($success, 'User login successfully');
+        } else {
+            return $this->sendError('Unauthorized' . ['error' => 'Unauthorized']);
+        }
+    }
+}
