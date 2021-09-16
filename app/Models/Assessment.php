d317e4a96407f49a7d4bec23c4d09d6bbbb89094
Syahrul Setiawan <monztergundul@gmail.com>
2021-09-16 14:55:33 +0700

    Initiate assessment and fix booking times migration

---
+++ app/Models/Assessment.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\HasMedia;
+
+class Assessment extends Model implements HasMedia
+{
+    use HasFactory, InteractsWithMedia;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'assess_id',
+        'user_id',
+        'point',
+        'expires_at',
+    ];
+
+    public function sectors()
+    {
+        return $this->hasMany('App\Models\AssessmentDetail');
+    }
+}
